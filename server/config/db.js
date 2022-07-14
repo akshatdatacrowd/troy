6f420163356f83509192865a933b58c8fd8a3198
Akshat Agarwal <akshatagarwal@Akshats-MacBook-Air.local>
2022-07-14 01:32:10 -0500

    Secondary Commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,8 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
-}
+  const conn = await mongoose.connect(process.env.MONGO_URI);
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+};
 
 module.exports = connectDB;
