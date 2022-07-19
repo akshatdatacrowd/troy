b746c8878eb5feebd3bcecaecab2c9edd6e2432e
Akshat Agarwal <63671921+akshatdatacrowd@users.noreply.github.com>
2022-07-18 22:29:01 -0500

    Created a boilerplate for future products

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
 
   return (
     <>
+      <Router>
       <Header />
-      <div className="container">
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<h1>About</h1>} />
+      </Routes>  
+      </Router>
+      
     </>
   );
 }
