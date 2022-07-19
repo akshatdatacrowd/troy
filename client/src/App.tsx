e0b83cadfcf24bdff317e246a4a4713382fffebc
Akshat Agarwal <63671921+akshatdatacrowd@users.noreply.github.com>
2022-07-18 21:27:01 -0500

    Added Redux to the front end

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,11 @@ import { useState } from "react";
 import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
       <Header />
       <div className="container">
-        <h1>Hello World</h1>
       </div>
     </>
   );
